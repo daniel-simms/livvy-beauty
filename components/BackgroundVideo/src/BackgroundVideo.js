ef5ac2d5c6bda3e506df6087ef9eb7806b8eb543
daniel-simms <danny_ghandi@hotmail.co.uk>
2021-04-02 14:09:25 +0100

    Removed css modules in favour of styled components

--- components/BackgroundVideo/src/BackgroundVideo.js
+++ components/BackgroundVideo/src/BackgroundVideo.js
@@ -1,20 +1,42 @@
 import React from "react";
-import styles from "./background-video.module.css";
+import styled from "styled-components";
 
 export default function BackgroundVideo({ src }) {
   return (
-    <div className={styles.container}>
-      <div className={styles.overlay} />
-      <video
-        loop
-        autoPlay
-        muted
-        playsInline
-        className={styles.video}
-      >
+    <Container>
+      <Overlay />
+      <Video loop autoPlay muted playsInline>
         <source src={src} />
-      </video>
-    </div>
+      </Video>
+    </Container>
   );
 }
 // TODO: Credit Pexels video + Sort something out with the video on smaller/ unsupported devices
+
+const Container = styled.div`
+  position: absolute;
+  height: calc(100vh - 120px);
+  width: 100%;
+  top: 120px;
+  left: 0;
+  overflow: hidden;
+`;
+
+const Video = styled.video`
+  position: fixed;
+  object-fit: cover;
+  right: 0;
+  top: 0;
+  min-width: 100%;
+  min-height: 100%;
+  z-index: -99999;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  background-color: black;
+  opacity: 0.5;
+  width: 100%;
+  height: 100%;
+  z-index: -999;
+`;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 
 import { LocationOn } from "@material-ui/icons";
-import { Button } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
 import styled from "styled-components";
 
 import Layout, { siteTitle } from "../components/Layout";
@@ -22,31 +22,55 @@ export default function Home({ video }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <BackgroundVideo src={video} />
 
-      <section>
-        <div>
-          <h1>
-            Beautician
-            <small>
-              <Icon fontSize="large" /> North London
-            </small>
-          </h1>
+      <VirticalCenter>
+        <Container>
+          <Title variant="h2" component="h1">Beautician</Title>
+          <Location variant="h4" component="h2">
+            <Icon fontSize="large" /> North London
+          </Location>
           <Link href="/treatments">
             <CTA variant="outlined">Get Pampered 💅🏻</CTA>
           </Link>
-        </div>
-      </section>
+        </Container>
+      </VirticalCenter>
 
-      <BackgroundVideo src={video} />
     </Layout>
   );
 }
+
+const VirticalCenter = styled.section`
+  display: grid;
+  place-items: center;
+  height: 70vh;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const Title = styled(Typography)`
+  color: #c83aa8;
+  margin: 0;
+`;
+
+const Location = styled(Typography)`
+  color: white;
+  margin: 0;
+  &&& {
+    margin-left: -5px;
+  }
+`;
 
 const CTA = styled(Button)`
   &&& {
     text-transform: capitalize;
     color: #c83aa8;
     border: 1px solid #c83aa8;
+    margin: 2rem 0;
   }
 `;
 
