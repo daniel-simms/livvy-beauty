1a6440c99454571a0deda5197ba6f16f099a3cd1
daniel-simms <danny_ghandi@hotmail.co.uk>
2021-04-02 21:10:33 +0100

    Changed some meta tags

--- components/Layout/src/Layout.js
+++ components/Layout/src/Layout.js
@@ -21,8 +21,10 @@ export default function Layout({ children }) {
           content="/images/livvy-logo-icon.png"
           key="image"
         />
-        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
-        <meta name="og:title" content={siteTitle} key="title" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
 
         <meta name="application-name" content="Livvy Beauty" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -30,7 +32,55 @@ export default function Layout({ children }) {
         <meta name="apple-mobile-web-app-title" content="Livvy Beauty" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
+        {/* <meta
+          name="msapplication-config"
+          content="icons/browserconfig.xml"
+        /> */}
+        <meta name="msapplication-TileColor" content="#c83aa8" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#c83aa8" />
 
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="icons/icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="icons/icon-72x72.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="icons/icon-72x72.png"
+        />
+        <link rel="manifest" href="/static/manifest.json" />
+        <link rel="mask-icon" href="icons/icon-72x72.png" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://livvy.beauty" />
+        <meta name="twitter:title" content="Livvy Beauty" />
+        <meta
+          name="twitter:description"
+          content="Olivia Callaghan - North London based beautician."
+        />
+        <meta
+          name="twitter:image"
+          content="https://livvy.beautyicons/android-chrome-192x192.png"
+        />
+        <meta name="twitter:creator" content="@DanielMarcSimms" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Livvy Beauty" />
+        <meta
+          property="og:description"
+          content="Olivia Callaghan - North London based beautician."
+        />
+        <meta property="og:site_name" content="Livvy Beauty" />
+        <meta property="og:url" content="https://livvy.beauty" />
       </Head>
       <Header />
       <Content>{children}</Content>
