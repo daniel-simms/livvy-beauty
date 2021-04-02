d07b54e85eeb3d4377758e2b4a0158f7f548f968
daniel-simms <danny_ghandi@hotmail.co.uk>
2021-04-02 01:19:23 +0100

    Added Reviews

---
+++ data/reviews.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    name: 'Danny Simms',
+    review: 'This girl is the best beauty therapist in the whole wide world! And she is super gorgeous too',
+    stars: 5
+  },
+  {
+    name: 'Freddy John',
+    review: 'The best, most relaxing experience, thanks for making me feel great',
+    stars: 5
+  },
+  {
+    name: 'Jennifer Pullen',
+    review: 'Brilliant treatment and super service',
+    stars: 5
+  },
+  {
+    name: 'Sally Jatta',
+    review: 'Very good, professional and friendly. Does a very good job. Very satisfied customer',
+    stars: 5
+  },
+  {
+    name: 'Fiona Campbell',
+    review: 'Brilliant facial, very helpful advice & skin feels great',
+    stars: 5
+  },
+  {
+    name: 'Sarah Jane Miles',
+    review: 'Fabulous service, great results. Will definitely be coming again',
+    stars: 5
+  },
+  {
+    name: 'Francesca Millar',
+    review: 'Excellent service. They are so experienced for every individuals problems / concerns and deal accordingly. The treatment is awesome, feel like heaven. Recommended to have all the treatments here',
+    stars: 5
+  },
+  {
+    name: 'Meena Khan',
+    review: 'Thanks for making me feel good. Always happy to come here. Thank you',
+    stars: 5
+  },
+  {
+    name: 'Sohail Ellahi',
+    review: 'Brilliant treatment and super service',
+    stars: 5
+  },
+];
--- pages/reviews.js
+++ pages/reviews.js
@@ -1,9 +1,63 @@
 import Layout from "../components/Layout";
+import {
+  Grid,
+  Card,
+  Avatar,
+  CardContent,
+  CardMedia,
+  IconButton,
+  CardHeader,
+} from "@material-ui/core/";
+import { Rating } from "@material-ui/lab/";
+import styled from "styled-components";
+import reviews from "../data/reviews";
 
 export default function Reviews() {
   return (
     <Layout>
-      <h1>Reviews</h1>
+      <h1 style={{ margin: 0 }}>Reviews</h1>
+
+      <ReviewList container spacing={3}>
+        {reviews.map((review) => (
+          <Grid item xs={12} md={6} lg={4}>
+            <Card>
+              <Reviewer
+                title={review.name}
+                subheader={
+                  <Rating name="read-only" value={5} size="small" readOnly />
+                }
+                avatar={
+                  <ReviewerAvatar aria-label="recipe">
+                    {review.name.charAt(0)}
+                  </ReviewerAvatar>
+                }
+              />
+              <CardContent>
+                <Review>{review.review}</Review>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </ReviewList>
+      <div style={{ height: 80, width: 80 }}></div>
     </Layout>
-  )
+  );
 }
+
+const ReviewList = styled(Grid)``;
+
+const Reviewer = styled(CardHeader)`
+  &&& {
+    padding-bottom: 0;
+  }
+`;
+
+const Review = styled.p`
+  margin: 0;
+`;
+
+const ReviewerAvatar = styled(Avatar)`
+  &&&{
+    background-color: #c83aa8;
+  }
+`;
